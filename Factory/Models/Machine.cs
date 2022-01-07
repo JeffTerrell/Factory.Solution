4c858a5bb31d4ec28a598223c716b6c877da212a
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-07 12:42:23 -0500

    add machine class and properties

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntitiesEngineer = new HashSet<EngineerMachine>();
+    }
+    public int MachineId {get; set; }
+    public string Name {get; set; } 
+    public string Description { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+    public DateTime InstallationDate {get; set;}
+    public DateTime InspectionDate { get; set; }
+    
+    public bool Operational { get; set; } = false;
+    public bool Malfunctioning { get; set; } = false;
+    public bool UnderRepair { get; set; } = false;
+
+    public virtual ICollection<EngineerMachine> JoinEntitiesEngineer { get; }
+  }
+}
