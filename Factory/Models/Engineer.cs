ec8c4ba3723b88ebed6e940dd966d13c99fb66d3
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-07 12:51:40 -0500

    add engineer class and properties

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntitiesMachine = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+    public DateTime LicenseRenewalDate {get; set;}
+
+    public bool Active { get; set; } = false;
+    public bool Idle { get; set; } = false;
+    public virtual ICollection<EngineerMachine> JoinEntitiesMachine { get; set; }
+  }
+}
