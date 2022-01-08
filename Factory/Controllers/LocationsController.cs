35917ccfa0c120f94845057ba57911b2e12c7cc9
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-07 19:29:06 -0500

    add locations edit/delete controllers/views & add engineer/machine buttons

--- Factory/Controllers/LocationsController.cs
+++ Factory/Controllers/LocationsController.cs
@@ -33,5 +33,87 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      var thisLocation = _db.Locations      
+      .Include(location => location.JoinEntitiesEngineer)
+      .ThenInclude(join => join.Engineer)
+      .Include(location => location.JoinEntitiesMachine)
+      .ThenInclude(join => join.Machine)
+      .FirstOrDefault(location => location.LocationId== id);
+      return View(thisLocation);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisLocation = _db.Locations.FirstOrDefault(location => location.LocationId == id);
+      return View(thisLocation);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Location location)
+    {
+      _db.Entry(location).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisLocation = _db.Locations.FirstOrDefault(location => location.LocationId == id);
+      return View(thisLocation);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisLocation = _db.Locations.FirstOrDefault(location => location.LocationId == id);
+      _db.Locations.Remove(thisLocation);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult AddEngineer(Location location, int EngineerId)
+    {
+      if (EngineerId != 0)
+      {
+      _db.EngineerLocation.Add(new EngineerLocation() { EngineerId = EngineerId , LocationId = location.LocationId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteEngineer(int joinId)
+    {
+      var joinEntry = _db.EngineerLocation.FirstOrDefault(entry => entry.EngineerLocationId == joinId);
+      _db.EngineerLocation.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Location location, int MachineId)
+    {
+      if (MachineId != 0)
+      {
+      _db.LocationMachine.Add(new LocationMachine() { MachineId = MachineId , LocationId = location.LocationId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteMachine(int joinId)
+    {
+      var joinEntry = _db.LocationMachine.FirstOrDefault(entry => entry.LocationMachineId == joinId);
+      _db.LocationMachine.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 } 
